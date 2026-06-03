dc915f6a5df4e49a95e26d13f66b8b9ce4677859
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2026-06-03 09:05:06 +0800

    Initial Commit

---
+++ swift-mail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-mail
+//
+//  Created by Declan Wade on 3/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-mail/swift_mailApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_mailApp.swift
+//  swift-mail
+//
+//  Created by Declan Wade on 3/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct swift_mailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
